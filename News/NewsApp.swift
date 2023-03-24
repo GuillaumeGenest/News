952c4dee7075a5d9abf4b71bf6672ad387ba038c
Guillaume Genest <guillaume-genest2@hotmail.fr>
2023-03-24 09:10:25 +0100

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Guillaume Genest on 24/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
